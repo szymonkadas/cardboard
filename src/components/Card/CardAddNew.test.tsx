765e24fc8b684349f9b028758ce3ef55948df96d
Szymon Kadaś <kadasszymon@gmail.com>
2023-12-04 11:22:15 +0100

    implemented unit tests for CardAddNew

--- src/components/Card/CardAddNew.test.tsx
+++ src/components/Card/CardAddNew.test.tsx
@@ -1,3 +1,25 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Mock, vi } from 'vitest'
+import { CardAddNew } from './CardAddNew'
+
 describe('CardAddNew component unit tests', () => {
-  // TODO: Add unit tests
+  let mockOnAddCard: Mock<any, any>
+  let cardAddNewPointer: HTMLButtonElement
+  beforeEach(() => {
+    mockOnAddCard = vi.fn()
+  })
+  test('Check if clicking on <CardAddNew /> fires onAddCard handler when component is not disabled', () => {
+    cardTestSetup(false)
+    expect(mockOnAddCard).toHaveBeenCalled()
+  })
+  test('Check  if clicking on <CardAddNew /> fires onAddCard handler when component is disabled', () => {
+    cardTestSetup(true)
+    expect(mockOnAddCard).not.toHaveBeenCalled()
+  })
+
+  function cardTestSetup(disabled: boolean) {
+    render(<CardAddNew onAddCard={mockOnAddCard} disabled={disabled} />)
+    cardAddNewPointer = screen.getByTestId('card-add-new')
+    fireEvent.click(cardAddNewPointer)
+  }
 })
